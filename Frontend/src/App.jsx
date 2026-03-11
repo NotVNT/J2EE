c4d189738050b1c3e7a1c6b763f7eaef93618534
Vinh Ngoc Tran <play13072004@gmail.com>
2026-03-11 19:54:19 +0700

    Dev (#5)
    * Merge pull request #1 from NotVNT/setup
    
    set up spring boot and react+vite
    
    * register & login
    
    * register_login
    
    * addCategory
    
    * addincome
    
    ---------
    
    Co-authored-by: quyenbu123 <caodangquyen.pt05@gmail.com>
    Co-authored-by: quyenbu123 <128795079+quyenbu123@users.noreply.github.com>

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/login" replace />;
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/signup" replace />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default App;
